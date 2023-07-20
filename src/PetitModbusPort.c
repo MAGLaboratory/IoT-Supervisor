b5d181cda00a4391b407249bb192733dfae32f4d
Brandon <blu006@ucr.edu>
2023-07-20 12:41:00 -0700

    Separated modbus functions.

--- src/PetitModbusPort.c
+++ src/PetitModbusPort.c
@@ -1,46 +1,40 @@
 #include "PetitModbusPort.h"
 #include <SI_EFM8BB1_Register_Enums.h>
 
-void PetitTimerStart()
+void PetitPortTimerStart()
 {
 	TL0 = (0x20 << TL0_TL0__SHIFT);
 	TCON_TR0 = true;
 	return;
 }
 
-void PetitTimerStop()
+void PetitPortTimerStop()
 {
 	TCON_TR0 = false;
 	TL0 = (0x20 << TL0_TL0__SHIFT);
 	return;
 }
 
-void PetitTxPinOn()
+void PetitPortTxPinOn()
 {
 	P0_B3 = true;
 	return;
 }
 
-void PetitTxPinOff()
+void PetitPortTxPinOff()
 {
 	P0_B3 = false;
 	return;
 }
 
-void PetitUartBegin()
+
+// the linker will warn about this function because it is used in an ISR,
+// but the ISR will only be called after this function executes.  So it's fine.
+void PetitPortUartBegin(unsigned char tx)
 {
-	P0_B3 = true;
+	PetitPortTxPinOn();
 	// no need to check here, this function should only be called by 
 	// petitmodbus on the first character anyway
-	unsigned char tx;
-	PetitUartTxBufferPop(&tx);
-	SBUF = tx;
-}
-
-void PetitUartTxBufferReset();
-{
-}
-
-void PetitUartRxBufferReset();
-{
+	SBUF0 = tx;
+	return;
 }
